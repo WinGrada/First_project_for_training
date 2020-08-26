21b09c87fbd477b53c72b73afd9f1add28d3b64e
Wingrada <wingrada@gmail.com>
2020-08-26 14:12:47 +0500

    Удалил ненужные активити, создал механику нанесения урона

--- app/src/main/java/uz/wingrada/myapplication/GameActivity.kt
+++ app/src/main/java/uz/wingrada/myapplication/GameActivity.kt
@@ -1,12 +1,41 @@
 package uz.wingrada.myapplication
 
+import android.bluetooth.BluetoothHeadset
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+
+
     }
+
+    private var health:Int = 100
+    private var damage:Int = 30
+    private var alive:Boolean = true
+
+    fun getDamage() {
+        if (damage > health){
+            health = 0
+            return
+        }
+        health -= damage
+    }
+
+
+
+    fun damageGround(view: View) {
+        getDamage()
+
+        if (health == 0){
+            GroundView.visibility = View.GONE
+        }
+    }
+
+
 }
--- app/src/main/java/uz/wingrada/myapplication/MainActivity.kt
+++ app/src/main/java/uz/wingrada/myapplication/MainActivity.kt
@@ -17,31 +17,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun toast(view: View){
-        val myToast = Toast.makeText(this, "Добро пожаловать в Driller", Toast.LENGTH_SHORT)
-
-        myToast.show()
-    }
-
-    fun count(view: View){
-        val countString = textCount.text.toString()
-
-        var count: Int = Integer.parseInt(countString)
-        count++
-
-        textCount.text = count.toString()
-    }
-
-    fun random(view: View){
-        val randomIntent = Intent(this, SecondActivity::class.java)
-        val countString = textCount.text.toString()
-        val count = Integer.parseInt(countString)
-
-        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
-
-        startActivity(randomIntent)
-    }
-
     fun enterToGameScene(view: View){
         val gameIntent = Intent(this, GameActivity::class.java)
         startActivity(gameIntent)
